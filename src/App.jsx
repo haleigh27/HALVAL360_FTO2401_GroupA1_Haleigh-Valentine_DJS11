d9ee2e6fc838d005c20811c8a8921d5578c2afd7
Haleigh <haleighvalentine@gmail.com>
2024-06-17 21:56:03 +0200

    Create page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import RootLayout from './pages/Layout/RootLayout';
+import Genre from './pages/Genre';
+import Library from './pages/Library';
+import Show from './pages/Show';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      }, // path: ""
+      { path: 'show/:showId', element: <Show /> },
+      { path: 'search', element: <Search /> },
+      { path: 'search/:genreId', element: <Genre /> },
+      {
+        path: 'search/:genreId/show/:showId',
+        element: <Show />,
+      },
+      { path: 'library', element: <Library /> },
+    ],
+  },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
