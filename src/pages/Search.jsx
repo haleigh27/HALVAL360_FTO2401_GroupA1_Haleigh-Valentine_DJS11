32d1ccc2715ff4f3e44e432e8add50a923293ec0
Haleigh <haleighvalentine@gmail.com>
2024-06-18 06:13:14 +0200

    Add genres to Search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,1 +1,29 @@
-export default function Search() {}
+import React from 'react';
+import { Link } from 'react-router-dom';
+import genres from '../genreTypes';
+
+import classes from './Search.module.css';
+
+export default function Search() {
+  return (
+    <>
+      <div>
+        <input type="text" />
+        <div>icon</div>
+      </div>
+      <div>
+        <h1>Genres</h1>
+        <ul className={classes.genreList}>
+          {genres.map((genre) => (
+            <li key={genre.id}>
+              <Link to={genre.id} className={classes.genreLink}>
+                <h2>{genre.title}</h2>
+                {console.log(genre)}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
