8a1f3721872baf57ad32d14503b1459c3b6c10fd
Haleigh <haleighvalentine@gmail.com>
2024-06-20 02:35:29 +0200

    Add Middlewear for local storage integration

---
+++ src/store/localStorage/loadState.js
@@ -0,0 +1,11 @@
+export default function loadState() {
+  try {
+    const serializedState = localStorage.getItem('reduxState');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+}
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import localStorageMiddleware from './localStorage/localStorageMiddleware';
+import loadState from './localStorage/loadState';
 import favouritesReducer from './favouritesSlice';
+
+const preloadedState = loadState();
 
 const store = configureStore({
   reducer: {
     favourites: favouritesReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
+  preloadedState,
 });
 
 export default store;
